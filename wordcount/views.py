1a241e2f1f63e978f28a7d63cea41611f5eae69f
Farmaan Khan <fa20408765@wipro.com>
2023-07-23 14:28:16 +0530

    This is my first commit

---
+++ wordcount/views.py
@@ -0,0 +1,10 @@
+import operator
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'home.html',{'Hithere':'This is me'})
+
+
